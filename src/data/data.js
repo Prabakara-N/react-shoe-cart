4ecaa5fd3e347b674f5566f7481b9253ee8f28c3
prabakaran <mspk028@gmail.com>
2023-03-21 16:36:40 +0530

    project setup

---
+++ src/data/data.js
@@ -0,0 +1,76 @@
+const shoes = [
+  {
+    id: 1,
+    title: "Nike",
+    image:
+      "https://res.cloudinary.com/daxmjqsy2/image/upload/v1679393675/blue-nike_bbosil.jpg",
+    description:
+      "Designed for comfortable wear for sports and street style, NIKE FOOTWEAR is always fun to wear. Upgrade in style with a wide range from the world’s leading and much-loved sports brand, NIKE.",
+    price: 1200,
+    originalPrize: 1499,
+    offers: "20%",
+    category: "nike",
+  },
+  {
+    id: 2,
+    title: "Nike",
+    image:
+      "https://res.cloudinary.com/daxmjqsy2/image/upload/v1679393717/black-nike_d8vqtn.jpg",
+    description:
+      "Designed for comfortable wear for sports and street style, NIKE FOOTWEAR is always fun to wear. Upgrade in style with a wide range from the world’s leading and much-loved sports brand, NIKE.",
+    price: 1200,
+    originalPrize: 1499,
+    offers: "20%",
+    category: "nike",
+  },
+  {
+    id: 3,
+    title: "Nike",
+    image:
+      "https://res.cloudinary.com/daxmjqsy2/image/upload/v1679393921/nike_red_vowraj.jpg",
+    description:
+      "Designed for comfortable wear for sports and street style, NIKE FOOTWEAR is always fun to wear. Upgrade in style with a wide range from the world’s leading and much-loved sports brand, NIKE.",
+    price: 1200,
+    originalPrize: 1499,
+    offers: "20%",
+    category: "nike",
+  },
+  {
+    id: 4,
+    title: "Nike",
+    image:
+      "https://res.cloudinary.com/daxmjqsy2/image/upload/v1679394144/nike_k0aizf.jpg",
+    description:
+      "Designed for comfortable wear for sports and street style, NIKE FOOTWEAR is always fun to wear. Upgrade in style with a wide range from the world’s leading and much-loved sports brand, NIKE.",
+    price: 1200,
+    originalPrize: 1499,
+    offers: "20%",
+    category: "nike",
+  },
+  {
+    id: 5,
+    title: "Nike",
+    image:
+      "https://res.cloudinary.com/daxmjqsy2/image/upload/v1679394202/nike-yellow_ejmyvm.jpg",
+    description:
+      "Designed for comfortable wear for sports and street style, NIKE FOOTWEAR is always fun to wear. Upgrade in style with a wide range from the world’s leading and much-loved sports brand, NIKE.",
+    price: 1200,
+    originalPrize: 1499,
+    offers: "20%",
+    category: "nike",
+  },
+  {
+    id: 6,
+    title: "Nike",
+    image:
+      "https://res.cloudinary.com/daxmjqsy2/image/upload/v1679394272/nike_casual_v2wbtl.jpg",
+    description:
+      "Designed for comfortable wear for sports and street style, NIKE FOOTWEAR is always fun to wear. Upgrade in style with a wide range from the world’s leading and much-loved sports brand, NIKE.",
+    price: 1200,
+    originalPrize: 1499,
+    offers: "20%",
+    category: "nike",
+  },
+];
+
+export default shoes;
